dedb9a8bf262b3a39883d8052db7ad15c271326c
sanchitagithub12 <sanchitagithub@gmail.com>
2021-09-02 10:30:04 +0530

    dadsda

--- components/Feature/Parallax.vue
+++ components/Feature/Parallax.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="banner-parallax-wrap" v-if="loaded">
+    <v-row>
     <parallax :speed-factor="0.2" direction="down">
       <!--svg width="100px" height="100px" class="parallax-vertical parallax-dot">
         <use xlink:href="/images/decoration/dot-deco.svg#dot" />
